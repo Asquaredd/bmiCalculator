c535db1cd982f541613f6e681cb17e2c9c9db904
AAdhikari <45696248+Asquaredd@users.noreply.github.com>
2023-06-07 17:48:23 -0700

    BMICalculator1.0
    working build 1.0

--- index.js
+++ index.js
@@ -6,6 +6,34 @@ function calculateBMI() {
     const heightInches = (height * 12);
     const bmi = weight / (heightInches * heightInches) * 703;
     document.getElementById("result").innerHTML = bmi.toFixed(2);
+    const resultContainer = document.querySelector('.result-container');
+    resultContainer.style.display = 'block';
+    resultContainer.classList.remove('reveal');
+    setTimeout(function() {resultContainer.classList.add('reveal');}, 10);
+  
+  
+    // Update color zone and arrow
+  const colorZone = document.querySelector('.color-zone');
+  const arrow = document.querySelector('.arrow');
+  
+  if (bmi < 18.5) {
+    resultContainer.classList.remove('green-zone', 'yellow-zone');
+    resultContainer.classList.add('red-zone');
+    arrow.style.marginLeft = 'calc(' + (bmi / 18.5) * 20 + '% - 5px)';
+  } else if (bmi >= 18.5 && bmi <= 20) {
+    resultContainer.classList.remove('red-zone', 'green-zone');
+    resultContainer.classList.add('yellow-zone');
+    arrow.style.marginLeft = 'calc(' + (bmi / 18.5) * 50 + '% - 5px)';
+  } else if (bmi > 20 && bmi <= 24.9) {
+    resultContainer.classList.remove('red-zone', 'yellow-zone');
+    resultContainer.classList.add('green-zone');
+    arrow.style.marginLeft = 'calc(' + (bmi / 18.5) * 70 + '% - 5px)';
+  }
+
+
+
+const calculateButton = document.getElementById('calculate-button');
+calculateButton.addEventListener('click', calculateBMI);
 }
 
 // script.js
